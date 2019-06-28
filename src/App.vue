f42e91ad6fc5c8af37e85a771cdb853d35e82330
Jonas Oesch <jonas.oesch@fhnw.ch>
2019-06-28 19:08:20 +0200

    Erster simpler Dreisatz-Rechner

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Comparison use="Dusche" kwh="20" v-bind:curKWh="curKWh" v-on:change="update">
+      {{text}}
+      </Comparison>
+    <Comparison use="Other" kwh="20" v-bind:curKWh="curKWh" v-on:change="update">
+      {{text}}
+      </Comparison>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Comparison from './components/Comparison.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Comparison
+  },
+  data: function() {
+      return {
+          curKWh: 1,
+          explanation: 'Wenn die Belegung bei {0}% liegt und Sie in einem {1} fahren.',
+          occupancy: 60,
+          traintype: 'Interregio'
+      }
+  },
+  computed: {
+    text: function () {
+        return this.explanation.replace("{0}", this.occupancy).replace("{1}", this.traintype)
+    },
+    formula: function() {
+        return this.value * (this.occupancy/60)
+    }
+  },
+  methods: {
+    update: function(event) {this.curKWh = event}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700');
+    body {
+        background-color: #454C4D;
+        color: #fff;
+        font-family: "Fira Sans", "Helvetica Neue", "Arial", sans-serif;
+    }
+
+    #app {
+        max-width: 600px;
+        margin: 0 auto;
+    }
 </style>
---
+++ src/components/Comparison.vue
@@ -0,0 +1,42 @@
+<template>
+    <article>
+        <h1>{{use}}</h1>
+        <h2><span class="value-label">Distanz:</span>&nbsp;<input type="text" v-model="value"/> km </h2>
+        <p class="explanation"><slot></slot></p>
+        <p>{{curKWh}}</p>
+    </article>
+</template>
+
+<script>
+export default {
+  name: 'Comparison',
+  props: ['use', 'kwh', 'curKWh'],
+  data: function() {
+    return {
+        currentKWh: this.kwh
+    }
+  },
+  computed: {
+      value: {
+        get: function () {
+          return parseInt(this.curKWh) / 2 
+        },
+        set: function (val) {
+           this.$emit('change', parseInt(val) ? parseInt(val)*2 : 0) 
+        }
+      }
+  }
+}
+</script>
+
+<style scoped>
+    .value-label {
+        font-family: serif;
+        font-style: italic;
+    }
+
+    .explanation {
+        font-family: serif;
+        font-style: italic;
+    }
+</style>
